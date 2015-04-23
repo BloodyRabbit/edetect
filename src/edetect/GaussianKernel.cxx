413a19e52954bd4333b1cf988c8e7be6df68e09c
Bloody.Rabbit <jan.bobek@gmail.com>
2015-04-23 18:24:53 +0200

    Add class GaussianKernel, DerivativeOfGaussianKernel, LaplacianOfGaussianKernel.

---
+++ src/edetect/GaussianKernel.cxx
@@ -0,0 +1,122 @@
+/** @file
+ * @brief Definition of GaussianKernel and its relatives.
+ *
+ * @author Jan Bobek
+ * @since 23th April 2015
+ */
+
+#include "edetect.hxx"
+#include "GaussianKernel.hxx"
+
+/*************************************************************************/
+/* GaussianKernel                                                        */
+/*************************************************************************/
+float*
+GaussianKernel::operator()(
+    unsigned int radius,
+    unsigned int& length
+    )
+{
+    const unsigned int origin = radius;
+    length = 2 * radius + 1;
+
+    const double sigma = radius / 2.5;
+    const double coef = -1.0 / (2.0 * sigma * sigma);
+
+    float* const kernel = (float*)malloc( length * sizeof(*kernel) );
+
+    float sum = kernel[origin] = 1.0f;
+    for( unsigned int i = 1, r2i = 1; i <= radius; r2i += 1 + 2 * i++ )
+        sum += 2.0f *
+            (kernel[origin - i] =
+             kernel[origin + i] =
+             exp( coef * r2i ));
+
+    for( unsigned int i = 0; i < length; ++i )
+        kernel[i] /= sum;
+
+    return kernel;
+}
+
+/*************************************************************************/
+/* DerivativeOfGaussianKernel                                            */
+/*************************************************************************/
+float*
+DerivativeOfGaussianKernel::operator()(
+    unsigned int radius,
+    unsigned int& length
+    )
+{
+    const unsigned int origin = radius;
+    length = 2 * radius + 1;
+
+    const double sigma = radius / 2.5;
+    const double coef = -1.0 / (2.0 * sigma * sigma);
+
+    float* const kernel = (float*)malloc( length * sizeof(*kernel) );
+
+    float sum = kernel[origin] = 0.0f;
+    for( unsigned int i = 1, r2i = 1; i <= radius; r2i += 1 + 2 * i++ )
+    {
+        kernel[origin + i] = exp( coef * r2i );
+        kernel[origin - i] = -kernel[origin + i];
+
+        sum += kernel[origin + i];
+    }
+
+    for( unsigned int i = 0; i < length; ++i )
+        kernel[i] /= sum;
+
+    return kernel;
+}
+
+/*************************************************************************/
+/* LaplacianOfGaussianKernel                                             */
+/*************************************************************************/
+float*
+LaplacianOfGaussianKernel::operator()(
+    unsigned int radius,
+    unsigned int& length
+    )
+{
+    const unsigned int stride = 2 * radius + 1;
+    const unsigned int origin = radius * (stride + 1);
+    length = stride * stride;
+
+    const double sigma = radius / 2.5;
+    const double coef1 = 2.0 * sigma * sigma;
+    const double coef2 = -1.0 / coef1;
+
+    float* const kernel = (float*)malloc( length * sizeof(*kernel) );
+
+    kernel[origin] = -coef1;
+    for( unsigned int i = 1, r2i = 1; i <= radius; r2i += 1 + 2 * i++ )
+    {
+        (kernel[origin - i] =
+         kernel[origin - i * stride] =
+         kernel[origin + i] =
+         kernel[origin + i * stride] =
+         (r2i - coef1) * exp( coef2 * r2i ));
+
+        (kernel[origin - i * (stride - 1)] =
+         kernel[origin - i * (stride + 1)] =
+         kernel[origin + i * (stride - 1)] =
+         kernel[origin + i * (stride + 1)] =
+         (2 * r2i - coef1) * exp( coef2 * (2 * r2i) ));
+
+        for( unsigned int j = i + 1, r2ij = r2i + j * j; j <= radius; r2ij += 1 + 2 * j++ )
+            (kernel[origin - i * stride - j] =
+             kernel[origin - i * stride + j] =
+             kernel[origin + i * stride - j] =
+             kernel[origin + i * stride + j] =
+
+             kernel[origin - j * stride - i] =
+             kernel[origin - j * stride + i] =
+             kernel[origin + j * stride - i] =
+             kernel[origin + j * stride + i] =
+
+             (r2ij - coef1) * exp( coef2 * r2ij ));
+    }
+
+    return kernel;
+}
---
+++ src/edetect/GaussianKernel.hxx
@@ -0,0 +1,77 @@
+/** @file
+ * @brief Declaration of class GaussianKernel and relatives.
+ *
+ * @author Jan Bobek
+ * @since 23th April 2015
+ */
+
+#ifndef GAUSSIAN_KERNEL_HXX__INCL__
+#define GAUSSIAN_KERNEL_HXX__INCL__
+
+/**
+ * @brief Generates a common 1D Gaussian kernel.
+ *
+ * @author Jan Bobek
+ */
+class GaussianKernel
+{
+public:
+    /**
+     * @brief Generates the 1D Gaussian kernel.
+     *
+     * @param[in] radius
+     *   Radius of the kernel to generate.
+     * @param[out] length
+     *   Length of the generated kernel.
+     *
+     * @return
+     *   The generated kernel.
+     */
+    float* operator()( unsigned int radius, unsigned int& length );
+};
+
+/**
+ * @brief Generates a 1D Derivative-of-Gaussian kernel.
+ *
+ * @author Jan Bobek
+ */
+class DerivativeOfGaussianKernel
+{
+public:
+    /**
+     * @brief Generates the 1D Derivativ-of-Gaussian kernel.
+     *
+     * @param[in] radius
+     *   Radius of the kernel to generate.
+     * @param[out] length
+     *   Length of the generated kernel.
+     *
+     * @return
+     *   The generated kernel.
+     */
+    float* operator()( unsigned int radius, unsigned int& length );
+};
+
+/**
+ * @brief Generates a 2D Laplacian-of-Gaussian kernel.
+ *
+ * @author Jan Bobek
+ */
+class LaplacianOfGaussianKernel
+{
+public:
+    /**
+     * @brief Generates the 2D Laplacian-of-Gaussian kernel.
+     *
+     * @param[in] radius
+     *   Radius of the kernel to generate.
+     * @param[out] length
+     *   Length of the generated kernel.
+     *
+     * @return
+     *   The generated kernel.
+     */
+    float* operator()( unsigned int radius, unsigned int& length );
+};
+
+#endif /* !GAUSSIAN_KERNEL_HXX__INCL__ */
